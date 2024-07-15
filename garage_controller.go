d127c7ff69b090d6c435d2b16b1c1c280f15afe2
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2024-07-15 00:37:50 -0600

    some progress

--- garage_controller.go
+++ garage_controller.go
@@ -1,1 +1,32 @@
 package main
+
+import qdb "github.com/rqure/qdb/src"
+
+type EventType int
+
+type IEvent interface {
+	GetType() EventType
+}
+
+type GarageController struct {
+	db     qdb.IDatabase
+	events chan IEvent
+}
+
+func NewGarageController(db qdb.IDatabase) *GarageController {
+	return &GarageController{
+		db: db,
+	}
+}
+
+func (gc *GarageController) Init() {
+
+}
+
+func (gc *GarageController) Deinit() {
+
+}
+
+func (gc *GarageController) DoWork() {
+
+}
